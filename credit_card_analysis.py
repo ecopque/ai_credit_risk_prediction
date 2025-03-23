ddd3165ea8b840d5f8921713da1ac66016b30bbc
Edson Copque <ecop@disroot.org>
2025-03-23 17:10:18 -0300

    feat: including

---
+++ credit_card_analysis.py
@@ -0,0 +1,9 @@
+# FILE: /credit_card_analysis.py
+
+# pip install pandas numpy scikit-learn matplotlib seaborn xlrd openpyxl joblib
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
