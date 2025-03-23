e73b4b5135640ec91675c1064ccd607088b244d5
Edson Copque <ecop@disroot.org>
2025-03-23 17:16:28 -0300

    update

--- credit_card_analysis.py
+++ credit_card_analysis.py
@@ -11,3 +11,5 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 import joblib
+
+pd = pd.read_excel('default of credit card clients.xls', header=1)
