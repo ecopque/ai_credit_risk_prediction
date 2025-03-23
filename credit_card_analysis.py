af15fe76d493f25de5611bc993545105016bc276
Edson Copque <ecop@disroot.org>
2025-03-23 18:22:22 -0300

    Update

--- credit_card_analysis.py
+++ credit_card_analysis.py
@@ -26,18 +26,29 @@ df = pd.read_excel('default of credit card clients.xls', header=1) #4:
 # [2.2. Initial data exploration]
 # Check the first few lines:
 print(df.head()) #5:
+
 # Information about columns and data types:
 print(df.info()) #6:
+
 # Check for missing values:
 print(df.isnull().sum()) #7:
+
 # Descriptive statistics:
 print(df.describe()) #8:
 
 # [2.3. Data Cleaning and Preprocessing]
-# Fill missing values ​​with mean (for numeric columns):
+# Handle missing values:
 df.fillna(df.mean(), inplace=True) #9:
-# Encode categorical variables
+
+# Encode categorical variables:
 df = pd.get_dummies(df, columns=['SEX', 'EDUCATION', 'MARRIAGE'], drop_first=True) #10:
+
 # Normalize/Standardize numeric variables:
-scaler = StandardScaler()
+scaler = StandardScaler() #11:
 df[['LIMIT_BAL', 'AGE']] = scaler.fit_transform(df[['LIMIT_BAL', 'AGE']]) #11:
+
+# Split data into training and testing:
+x = df.drop('default payment next month', axis=1) #12:
+y = df['default payment next month'] #12:
+# Spliting:
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42) #13:
