5c97c2ca9bad3d28791f6bb921689f5904c2af71
Edson Copque <ecop@disroot.org>
2025-03-23 18:31:45 -0300

    Update.

--- credit_card_analysis.py
+++ credit_card_analysis.py
@@ -19,36 +19,39 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 import joblib
 
-# [2.1. Loading the data]
-# Upload the xls file:
+# [2.2. Loading the data]
+# [2.2.1. Upload the xls file]:
 df = pd.read_excel('default of credit card clients.xls', header=1) #4:
 
-# [2.2. Initial data exploration]
-# Check the first few lines:
+# [2.3. Initial data exploration]
+# [2.3.1. Check the first few lines]:
 print(df.head()) #5:
 
-# Information about columns and data types:
+# [2.3.2. Information about columns and data types]:
 print(df.info()) #6:
 
-# Check for missing values:
+# [2.3.3. Check for missing values]:
 print(df.isnull().sum()) #7:
 
-# Descriptive statistics:
+# [2.3.4. Descriptive statistics]:
 print(df.describe()) #8:
 
-# [2.3. Data Cleaning and Preprocessing]
-# Handle missing values:
+# [3. DATA CLEANING AND PROCESSING]
+# [3.1. Handle missing values]:
 df.fillna(df.mean(), inplace=True) #9:
 
-# Encode categorical variables:
+# [3.2. Encode categorical variable]:
 df = pd.get_dummies(df, columns=['SEX', 'EDUCATION', 'MARRIAGE'], drop_first=True) #10:
 
-# Normalize/Standardize numeric variables:
+# [3.3. Normalize/Standardize numeric variables]:
 scaler = StandardScaler() #11:
 df[['LIMIT_BAL', 'AGE']] = scaler.fit_transform(df[['LIMIT_BAL', 'AGE']]) #11:
 
-# Split data into training and testing:
+# [3.4. Split data into training and testing]:
 x = df.drop('default payment next month', axis=1) #12:
 y = df['default payment next month'] #12:
-# Spliting:
+# [3.4.1. Spliting]:
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42) #13:
+
+# [4. MODELING]
+# 
